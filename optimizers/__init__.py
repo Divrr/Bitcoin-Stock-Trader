df6a4808c91982b52d8328cca2005e097f388a61
Divrr <96827300+Divrr@users.noreply.github.com>
2025-04-29 15:31:10 +0800

    Implement CCS in main

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -2,6 +2,7 @@ from .ppso import PPSO
 from .hgsa import HGSA
 from .igwo import IGWO
 from .aco import ACO
+from .ccs import CCS
 from .base import Optimizer
 
-__all__ = ['PPSO', 'HGSA', 'IGWO', 'ACO', 'Optimizer']
+__all__ = ['PPSO', 'HGSA', 'IGWO', 'ACO', 'CCS', 'Optimizer']
